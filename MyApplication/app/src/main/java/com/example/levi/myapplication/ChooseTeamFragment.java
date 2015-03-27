876275c3a208bfc831a374d3920d533190c8a31d
Levi <leviovasconcelos@gmail.com>
2015-03-27 02:38:39 -0300

    adicionando gradle-wrapper.jar

--- MyApplication/app/src/main/java/com/example/levi/myapplication/ChooseTeamFragment.java
+++ MyApplication/app/src/main/java/com/example/levi/myapplication/ChooseTeamFragment.java
@@ -1,11 +1,15 @@
 package com.example.levi.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -13,10 +17,12 @@ import android.view.ViewGroup;
  */
 public class ChooseTeamFragment
         extends android.support.v4.app.Fragment
-{
+        implements ViewPager.PageTransformer, View.OnClickListener {
 
     private TeamAdapter teamAdapter;
+    private ViewPager uniformPager;
     private ViewPager teamPager;
+
     @Override
     public void onCreate(Bundle savedInstanceStates) {
         super.onCreate(savedInstanceStates);
@@ -27,15 +33,74 @@ public class ChooseTeamFragment
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_teams, container, false);
+
         teamAdapter = new TeamAdapter(getActivity());
         teamPager = (ViewPager) layout.findViewById(R.id.teamViewPager);
+        uniformPager = (ViewPager) layout.findViewById(R.id.uniformViewPager);
+
+        /**
+         * ****************** Buttons on click listeners *******************
+         */
+
+        //Team Buttons
+        Button nextTeamButton = (Button) layout.findViewById(R.id.nextTeam);
+        nextTeamButton.setOnClickListener(this);
+        Button previousTeamButton = (Button) layout.findViewById(R.id.previousTeam);
+        previousTeamButton.setOnClickListener(this);
+
+        //Uniform Buttons
+        Button nextUniformButton = (Button) layout.findViewById(R.id.nextUniform);
+        nextUniformButton.setOnClickListener(this);
+        Button previousUniformButton = (Button) layout.findViewById(R.id.previousUniform);
+        previousUniformButton.setOnClickListener(this);
+
+
+
+
         teamPager.setAdapter(teamAdapter);
-        teamPager.setOffscreenPageLimit(7);
-        this.teamPager.setPageMargin(-(int)(0.12D * 3));
-        //this.teamPager.setPageTransformer(true, this);
+        teamPager.setPageTransformer(true, this);
 
 
         return layout;
     }
 
+    public void onClick(View view) {
+
+        if (view.equals(getView().findViewById(R.id.nextTeam))) {
+            teamPager.setCurrentItem(teamPager.getCurrentItem() + 1, true);
+            return;
+            //((ViewPager) getView().findViewById(R.id.teamViewPager)).setCurrentItem();
+        }
+        if(view.equals(getView().findViewById(R.id.previousTeam))) {
+            teamPager.setCurrentItem(teamPager.getCurrentItem() - 1, true);
+            return;
+        }
+
+        if(view.equals(getView().findViewById(R.id.previousUniform))) {
+            uniformPager.setCurrentItem(uniformPager.getCurrentItem() - 1, true);
+            return;
+        }
+        if(view.equals(getView().findViewById(R.id.nextUniform))) {
+            uniformPager.setCurrentItem(uniformPager.getCurrentItem() + 1, true);
+            return;
+        }
+
+    }
+
+
+    public void transformPage(View page, float relPos) { //Animação de mudança, escala e transparencia
+        if (relPos < -1 || relPos > 1) { //Saiu da tela
+            page.setAlpha(0.0F);
+            return;
+        } else if (relPos <= 1.0F) { //Mudando...
+            //Escala
+            float scale = Math.max(0.6F, 1.0F - Math.abs(relPos));
+            page.setScaleX(scale);
+            page.setScaleY(scale);
+            //Transparencia
+            float transparency = Math.max(0.6F,1.0F - Math.abs(relPos));
+            page.setAlpha(transparency);
+            return;
+        }
+    }
 }
